5910f055fd5a5b89e7f2f1194d67bfe9cc6d3a82
unknown <quynhlt@qsq-PC.qsoft.com.vn>
2013-05-31 13:45:02 +0700

    red testReturnOfEmptyString

---
+++ StringCalculatorDay05/src/main/Calculator.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author quynhlt
+ *
+ */
+public class Calculator {
+
+	public static Object add(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
---
+++ StringCalculatorDay05/src/test/CalculatorTest.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.assertEquals;
+import main.Calculator;
+
+import org.junit.Test;
+
+/**
+ * @author quynhlt
+ * 
+ */
+public class CalculatorTest {
+	/*
+	 * The method can take 0, 1 or 2 numbers, and will return their sum (for an
+	 * empty string it will return 0) for example “” or “1” or “1,2”
+	 */
+	@Test
+	public void testReturnOfEmptyString() {
+		assertEquals(0, Calculator.add(""));
+	}
+
+}
