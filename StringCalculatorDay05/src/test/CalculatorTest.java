d1b3689b5ac185d54fd0816d83d680291c6ea232
unknown <quynhlt@qsq-PC.qsoft.com.vn>
2013-05-31 14:10:32 +0700

    red

--- StringCalculatorDay05/src/test/CalculatorTest.java
+++ StringCalculatorDay05/src/test/CalculatorTest.java
@@ -47,5 +47,10 @@ public class CalculatorTest {
 	public void testReturnOfNumbersWithCommasAndNewline() {
 		assertEquals(6, Calculator.add("1\n2,3"));
 	}
+
+	@Test
+	public void testReturnOfDifferentDelimiters() {
+		assertEquals(3, Calculator.add("//;\n1;2"));
+	}
 }
 // https://github.com/quynhlt/stringcaculator
