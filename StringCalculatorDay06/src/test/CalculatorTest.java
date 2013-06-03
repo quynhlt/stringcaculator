b39af736ac8bde11423d290b319f4ed9509740c5
unknown <quynhlt@qsq-PC.qsoft.com.vn>
2013-06-03 13:54:26 +0700

    red

--- StringCalculatorDay06/src/test/CalculatorTest.java
+++ StringCalculatorDay06/src/test/CalculatorTest.java
@@ -8,9 +8,13 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
-
 	@Test
 	public void testReturnOfEmptyString() {
 		assertEquals(0, Calculator.add(""));
+	}
+
+	@Test
+	public void testReturnOfOneNumber() {
+		assertEquals(1, Calculator.add("1"));
 	}
 }
