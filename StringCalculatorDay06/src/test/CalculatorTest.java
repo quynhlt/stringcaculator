af8f63b3fe9d2bbcd1d48cfdc82902e8d6e1c0a9
unknown <quynhlt@qsq-PC.qsoft.com.vn>
2013-06-03 13:55:49 +0700

    red

--- StringCalculatorDay06/src/test/CalculatorTest.java
+++ StringCalculatorDay06/src/test/CalculatorTest.java
@@ -17,4 +17,9 @@ public class CalculatorTest {
 	public void testReturnOfOneNumber() {
 		assertEquals(1, Calculator.add("1"));
 	}
+
+	@Test
+	public void testReturnOfTwoNumberWithCommas() {
+		assertEquals(3, Calculator.add("1,2"));
+	}
 }
