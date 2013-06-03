b3cff0fe637199e88206a1a37c2596a13c0fd5f7
unknown <quynhlt@qsq-PC.qsoft.com.vn>
2013-06-03 14:27:29 +0700

    red

--- StringCalculatorDay06/src/test/CalculatorTest.java
+++ StringCalculatorDay06/src/test/CalculatorTest.java
@@ -58,8 +58,14 @@ public class CalculatorTest {
 	public void testReturnOfNumberBiggerThan1000() {
 		assertEquals(1, Calculator.add("1,1212"));
 	}
+
 	@Test
 	public void testReturnOfMultiNumbersBiggerThan1000() {
 		assertEquals(1, Calculator.add("1,123123,123123"));
+	}
+
+	@Test
+	public void testReturnOfDelimWithAnyLength() {
+		assertEquals(3, Calculator.add("//[,,,]\n1,,,2"));
 	}
 }
