a6bd99750e1a6b6c4fd50a5e9512c707331bb8a5
unknown <quynhlt@qsq-PC.qsoft.com.vn>
2013-06-04 14:01:02 +0700

    clean

--- StringCalculatorDay07/src/main/Calculator.java
+++ StringCalculatorDay07/src/main/Calculator.java
@@ -10,24 +10,24 @@ package main;
 public class Calculator {
 
 	private static final String COMMAS = ",";
+	private static final String OR = "|";
+	private static final String NEWLINE = "\n";
 
 	public static int add(String number) {
 		int result = 0;
 		if (number.isEmpty()) {
 			result = 0;
-		} else if (number.contains(COMMAS)) {
+		} else {
 			String[] tokens = getToken(number);
 			for (String token : tokens) {
 				result += toInt(token);
 			}
-		} else {
-			result = Integer.parseInt(number);
 		}
 		return result;
 	}
 
 	private static String[] getToken(String number) {
-		return number.split(COMMAS+"|\n");
+		return number.split(COMMAS + OR + NEWLINE);
 	}
 
 	private static int toInt(String number) {
