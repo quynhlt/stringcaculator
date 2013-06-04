2742fffab19ec47a827d5634cda170b66be9f375
unknown <quynhlt@qsq-PC.qsoft.com.vn>
2013-06-04 13:54:32 +0700

    green

--- StringCalculatorDay07/src/main/Calculator.java
+++ StringCalculatorDay07/src/main/Calculator.java
@@ -13,6 +13,9 @@ public class Calculator {
 		int result = 0;
 		if (number.isEmpty()) {
 			result = 0;
+		} else if (number.contains(",")) {
+			String[] tokens = number.split(",");
+			result = Integer.parseInt(tokens[0]) + Integer.parseInt(tokens[1]);
 		} else {
 			result = Integer.parseInt(number);
 		}
