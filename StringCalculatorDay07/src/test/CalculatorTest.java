49d440f9bc583b90fc0181ca3a4cad4a7ecbaf1e
unknown <quynhlt@qsq-PC.qsoft.com.vn>
2013-06-04 14:01:47 +0700

    red

--- StringCalculatorDay07/src/test/CalculatorTest.java
+++ StringCalculatorDay07/src/test/CalculatorTest.java
@@ -39,5 +39,8 @@ public class CalculatorTest {
 	public void testReturnOfNumbersWithCommasAndNewline() {
 		assertEquals(6, Calculator.add("1\n2,3"));
 	}
-
+	@Test
+	public void testReturnOfOtherDelimiters() {
+		assertEquals(3, Calculator.add("//;\n1;2"));
+	}
 }
